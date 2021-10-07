e058b7a028f2130eeb40bce7ac5f528a395a41ab
Yasmine De Wolf <yasmine.de.wolf@student.ehb.be>
2021-10-07 20:48:15 +0200

    Styled the footer component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab, faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Home from './views/Home';
 
 library.add(fab, faFacebookF, faTwitter, faInstagram);
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
       <Footer />
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,59 +1,78 @@
-import React from 'react';
+import React from "react";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { ReactComponent as Logo } from '../assets/images/logo.svg';
-import { ReactComponent as LocationIcon } from '../assets/images/icon-location.svg';
-import { ReactComponent as PhoneIcon } from '../assets/images/icon-phone.svg';
-import { ReactComponent as MailIcon } from '../assets/images/icon-email.svg';
+import Logo from "../assets/images/logo.svg";
+import { ReactComponent as LocationIcon } from "../assets/images/icon-location.svg";
+import { ReactComponent as PhoneIcon } from "../assets/images/icon-phone.svg";
+import { ReactComponent as MailIcon } from "../assets/images/icon-email.svg";
 
 const Footer = () => {
     return (
-        <div>
-            <div className="logo-container">
-                <Logo />
-            </div>
-            <div className="footer-content">
-                <div className="location">
-                    <LocationIcon />
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua
-                    </p>
+        <footer className="footer">
+            <div className="container">
+                <div className="early-access">
+                    <h2>Get early access today</h2>
+                    <p>It only takes a minute to sign up and our free starter tier is extremely generous. If you have any 
+  questions, our support team would be happy to help you.</p>
+                    <form>
+                        <input type="email" id="email" name="email" placeholder="email@example.com"/>
+                        <button type="submit" className="btn btn-primary">Get Started For Free</button>
+                    </form>
                 </div>
-                <div className="contact-info">
-                    <div>
-                        <PhoneIcon />
-                        <p>+1-543-123-4567</p>
+                <div className="logo-container">
+                    <img src={Logo} alt="" />
+                </div>
+                <div className="footer-content">
+                    <div className="company-info">
+                        <div className="location">
+                            <LocationIcon className="icon" />
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua
+                            </p>
+                        </div>
+                        <div className="contact-info">
+                            <div>
+                                <PhoneIcon className="icon" />
+                                <p>+1-543-123-4567</p>
+                            </div>
+                            <div>
+                                <MailIcon className="icon" />
+                                <p>example@fylo.com</p>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <MailIcon />
-                        <p>example@fylo.com</p>
+                    <div className="footer-nav">
+                        <ul className="">
+                            <li>About Us</li>
+                            <li>Jobs</li>
+                            <li>Press</li>
+                            <li>Blog</li>
+                        </ul>
+                    </div>
+                    <div className="extra-info">
+                        <ul className="">
+                            <li>Contact Us</li>
+                            <li>Terms</li>
+                            <li>Privacy</li>
+                        </ul>
+                    </div>
+                    <div className="social-links">
+                        <a href="#" className="icon">
+                            <FontAwesomeIcon icon={["fab", "facebook-f"]} />
+                        </a>
+                        <a href="#" className="icon">
+                            <FontAwesomeIcon icon={["fab", "twitter"]} />
+                        </a>
+                        <a href="#" className="icon">
+                            <FontAwesomeIcon icon={["fab", "instagram"]} />
+                        </a>
                     </div>
                 </div>
-                <div className="footer-nav">
-                    <ul>
-                        <li>About Us</li>
-                        <li>Jobs</li>
-                        <li>Press</li>
-                        <li>Blog</li>
-                    </ul>
-                </div>
-                <div className="extra-info">
-                    <ul>
-                        <li>Contact Us</li>
-                        <li>Terms</li>
-                        <li>Privacy</li>
-                    </ul>
-                </div>
-                <div className="social-links">
-                    <a href="#" className="icon"><FontAwesomeIcon icon={['fab', 'facebook-f']} /></a>
-                    <a href="#" className="icon"><FontAwesomeIcon icon={['fab', 'twitter']} /></a>
-                    <a href="#" className="icon"><FontAwesomeIcon icon={['fab', 'instagram']} /></a>
-                </div>
             </div>
-        </div>
+        </footer>
     );
 };
 
