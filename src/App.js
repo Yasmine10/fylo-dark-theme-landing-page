596f17afa0280c1b5305abd459447f111151ee02
Yasmine De Wolf <yasmine.de.wolf@student.ehb.be>
2021-10-08 15:42:18 +0200

    added subscription box (html + styling)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab, faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+    fab,
+    faFacebookF,
+    faTwitter,
+    faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './views/Home';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./views/Home";
 
 library.add(fab, faFacebookF, faTwitter, faInstagram);
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Home />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <main>
+                <Home />
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,72 +7,69 @@ import { ReactComponent as LocationIcon } from "../assets/images/icon-location.s
 import { ReactComponent as PhoneIcon } from "../assets/images/icon-phone.svg";
 import { ReactComponent as MailIcon } from "../assets/images/icon-email.svg";
 
+import Subscription from '../components/home/Subscription';
+
 const Footer = () => {
     return (
-        <footer className="footer">
-            <div className="container">
-                <div className="early-access">
-                    <h2>Get early access today</h2>
-                    <p>It only takes a minute to sign up and our free starter tier is extremely generous. If you have any 
-  questions, our support team would be happy to help you.</p>
-                    <form>
-                        <input type="email" id="email" name="email" placeholder="email@example.com"/>
-                        <button type="submit" className="btn btn-primary">Get Started For Free</button>
-                    </form>
-                </div>
-                <div className="logo-container">
-                    <img src={Logo} alt="" />
-                </div>
-                <div className="footer-content">
-                    <div className="company-info">
-                        <div className="location">
-                            <LocationIcon className="icon" />
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit, sed do eiusmod tempor
-                                incididunt ut labore et dolore magna aliqua
-                            </p>
-                        </div>
-                        <div className="contact-info">
-                            <div>
-                                <PhoneIcon className="icon" />
-                                <p>+1-543-123-4567</p>
+        <div className="footer-wrapper">
+           <Subscription />
+           <div className="background"></div>
+            <footer className="footer">
+                <div className="container">
+                    <div className="logo-container">
+                        <img src={Logo} alt="" />
+                    </div>
+                    <div className="footer-content">
+                        <div className="company-info">
+                            <div className="location">
+                                <LocationIcon className="icon" />
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua
+                                </p>
                             </div>
-                            <div>
-                                <MailIcon className="icon" />
-                                <p>example@fylo.com</p>
+                            <div className="contact-info">
+                                <div>
+                                    <PhoneIcon className="icon" />
+                                    <p>+1-543-123-4567</p>
+                                </div>
+                                <div>
+                                    <MailIcon className="icon" />
+                                    <p>example@fylo.com</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="footer-nav">
-                        <ul className="">
-                            <li>About Us</li>
-                            <li>Jobs</li>
-                            <li>Press</li>
-                            <li>Blog</li>
-                        </ul>
-                    </div>
-                    <div className="extra-info">
-                        <ul className="">
-                            <li>Contact Us</li>
-                            <li>Terms</li>
-                            <li>Privacy</li>
-                        </ul>
-                    </div>
-                    <div className="social-links">
-                        <a href="#" className="icon">
-                            <FontAwesomeIcon icon={["fab", "facebook-f"]} />
-                        </a>
-                        <a href="#" className="icon">
-                            <FontAwesomeIcon icon={["fab", "twitter"]} />
-                        </a>
-                        <a href="#" className="icon">
-                            <FontAwesomeIcon icon={["fab", "instagram"]} />
-                        </a>
+                        <div className="footer-nav">
+                            <ul className="">
+                                <li>About Us</li>
+                                <li>Jobs</li>
+                                <li>Press</li>
+                                <li>Blog</li>
+                            </ul>
+                        </div>
+                        <div className="extra-info">
+                            <ul className="">
+                                <li>Contact Us</li>
+                                <li>Terms</li>
+                                <li>Privacy</li>
+                            </ul>
+                        </div>
+                        <div className="social-links">
+                            <a href="#" className="icon">
+                                <FontAwesomeIcon icon={["fab", "facebook-f"]} />
+                            </a>
+                            <a href="#" className="icon">
+                                <FontAwesomeIcon icon={["fab", "twitter"]} />
+                            </a>
+                            <a href="#" className="icon">
+                                <FontAwesomeIcon icon={["fab", "instagram"]} />
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </footer>
+            </footer>
+        </div>
     );
 };
 
