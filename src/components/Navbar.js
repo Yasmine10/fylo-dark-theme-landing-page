57dd5a58e27efc147ba9908a55af4e33bb3febde
Yasmine De Wolf <yasmine.de.wolf@student.ehb.be>
2021-10-08 18:24:47 +0200

    fixing background color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,17 @@ import Logo from "../assets/images/logo.svg";
 
 const Navbar = () => {
     return (
-        <nav className="navbar container">
-            <div className="logo-container">
-                <img src={Logo} alt="" />
+        <nav className="navbar-wrapper">
+            <div className="navbar container">
+                <div className="logo-container">
+                    <img src={Logo} alt="" />
+                </div>
+                <ul className="nav-items">
+                    <li className="nav-item">Features</li>
+                    <li className="nav-item">Team</li>
+                    <li className="nav-item">Sign In</li>
+                </ul>
             </div>
-            <ul className="nav-items">
-                <li className="nav-item">Features</li>
-                <li className="nav-item">Team</li>
-                <li className="nav-item">Sign In</li>
-            </ul>
         </nav>
     )
 }
--- src/components/home/Testimonials.js
+++ src/components/home/Testimonials.js
@@ -7,46 +7,48 @@ import profile3 from "../../assets/images/profile-3.jpg";
 
 const Testimonials = () => {
     return (
-        <div className="testimonials container">
-            <div className="quotes-img">
-                <img src={quotes} alt="" />
+        <div className="testimonials">
+            <div className="container">
+                <div className="quotes-img">
+                    <img src={quotes} alt="" />
+                </div>
+                <blockquote>
+                    <p>
+                        Fylo has improved our team productivity by an order of
+                        magnitude. Since making the switch our team has become a
+                        well-oiled collaboration machine.
+                    </p>
+                    <div className="client-info">
+                            <img src={profile1} alt="" />
+                            <h6>Satish Patel</h6>
+                            <small>Founder &amp; CEO, Huddle</small>
+                    </div>
+                </blockquote>
+                <blockquote>
+                    <p>
+                        Fylo has improved our team productivity by an order of
+                        magnitude. Since making the switch our team has become a
+                        well-oiled collaboration machine.
+                    </p>
+                    <div className="client-info">
+                            <img src={profile2} alt="" />
+                            <h6>Bruce McKenzie</h6>
+                            <small>Founder &amp; CEO, Huddle</small>
+                    </div>
+                </blockquote>
+                <blockquote>
+                    <p>
+                        Fylo has improved our team productivity by an order of
+                        magnitude. Since making the switch our team has become a
+                        well-oiled collaboration machine.
+                    </p>
+                    <div className="client-info">
+                            <img src={profile3} alt="" />
+                            <h6>Iva Boyd</h6>
+                            <small>Founder &amp; CEO, Huddle</small>
+                    </div>
+                </blockquote>
             </div>
-            <blockquote>
-                <p>
-                    Fylo has improved our team productivity by an order of
-                    magnitude. Since making the switch our team has become a
-                    well-oiled collaboration machine.
-                </p>
-                <div className="client-info">
-                        <img src={profile1} alt="" />
-                        <h6>Satish Patel</h6>
-                        <small>Founder &amp; CEO, Huddle</small>
-                </div>
-            </blockquote>
-            <blockquote>
-                <p>
-                    Fylo has improved our team productivity by an order of
-                    magnitude. Since making the switch our team has become a
-                    well-oiled collaboration machine.
-                </p>
-                <div className="client-info">
-                        <img src={profile2} alt="" />
-                        <h6>Bruce McKenzie</h6>
-                        <small>Founder &amp; CEO, Huddle</small>
-                </div>
-            </blockquote>
-            <blockquote>
-                <p>
-                    Fylo has improved our team productivity by an order of
-                    magnitude. Since making the switch our team has become a
-                    well-oiled collaboration machine.
-                </p>
-                <div className="client-info">
-                        <img src={profile3} alt="" />
-                        <h6>Iva Boyd</h6>
-                        <small>Founder &amp; CEO, Huddle</small>
-                </div>
-            </blockquote>
         </div>
     );
 };
